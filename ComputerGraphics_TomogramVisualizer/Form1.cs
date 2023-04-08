629fecb1c7bf874abf36d7d55b2ddac502b935dc
Julia Kurdina <kurdina.00@gmail.com>
2023-04-09 00:16:30 +0300

    smth add

--- ComputerGraphics_TomogramVisualizer/Form1.cs
+++ ComputerGraphics_TomogramVisualizer/Form1.cs
@@ -8,60 +8,73 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace TomogramVisualizer
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    //создаем и инициализируем объект
+    //Bin bin;
+
+    // чтобы не запускать отрисовку пока не загружены данные
+    bool loaded = false;
+
+    int currentLayer = 0;
+    bool needReload = false;
+    public Form1()
     {
-        //создаем и инициализируем объект
-        //Bin bin;
-
-        // чтобы не запускать отрисовку пока не загружены данные
-        bool loaded = false;
-
-        int currentLayer = 0;
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-            OpenFileDialog dialog = new OpenFileDialog();
-
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                string str = dialog.FileName;
-
-
-                //bin.readBIN(str);
-                Bin.readBIN(str);
-
-
-                View.SetupView(glControl1.Width, glControl1.Height);
-
-                loaded = true;
-
-                glControl1.Invalidate();
-
-                trackBar1.Maximum = Bin.Z - 1;
-            }
-        }
-
-        private void glControl1_Paint(object sender, PaintEventArgs e)
-        {
-            if (loaded)
-            {
-                View.DrawQuads(currentLayer);
-                glControl1.SwapBuffers();
-            }
-        }
-
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            currentLayer = trackBar1.Value;
-            glControl1.Invalidate();
-        }
+      InitializeComponent();
     }
+
+    private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+
+      OpenFileDialog dialog = new OpenFileDialog();
+
+      if (dialog.ShowDialog() == DialogResult.OK)
+      {
+        string str = dialog.FileName;
+
+
+        //bin.readBIN(str);
+        Bin.readBIN(str);
+
+
+        View.SetupView(glControl1.Width, glControl1.Height);
+
+        loaded = true;
+
+        glControl1.Invalidate();
+
+        trackBar1.Maximum = Bin.Z - 1;
+      }
+    }
+
+    private void glControl1_Paint(object sender, PaintEventArgs e)
+    {
+      if (loaded)
+      {
+        //View.DrawQuads(currentLayer);
+        if (needReload)
+        {
+          View.generateTextureImage(currentLayer);
+          View.Load2DTexture();
+          needReload = false;
+        }
+        View.DrawTexture();
+        glControl1.SwapBuffers();
+      }
+    }
+
+    private void trackBar1_Scroll(object sender, EventArgs e)
+    {
+      currentLayer = trackBar1.Value;
+      glControl1.Invalidate();
+      needReload = true;
+    }
+
+    
+
+  }
 }
--- ComputerGraphics_TomogramVisualizer/Tomogram.cs
+++ ComputerGraphics_TomogramVisualizer/Tomogram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -9,97 +10,150 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenTK.Graphics.OpenGL;
 
+
+
 namespace TomogramVisualizer
 {
-    public static class Bin
+  public static class Bin
+  {
+    public static int X, Y, Z;
+
+    public static short[] array;
+
+    public static void readBIN(string path)
     {
-        public static int X, Y, Z;
 
-        public static short[] array;
+      if (File.Exists(path))
+      {
+        BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
 
-        public static void readBIN(string path)
+        //size of tomogram
+        X = reader.ReadInt32();
+        Y = reader.ReadInt32();
+        Z = reader.ReadInt32();
+
+        //data array
+        int arraySize = X * Y * Z;
+        array = new short[arraySize];
+
+        for (int i = 0; i < arraySize; i++)
         {
-
-            if (File.Exists(path))
-            {
-                BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
-
-                //size of tomogram
-                X = reader.ReadInt32();
-                Y = reader.ReadInt32();
-                Z = reader.ReadInt32();
-
-                //data array
-                int arraySize = X * Y * Z;
-                array = new short[arraySize];
-
-                for (int i = 0; i < arraySize; i++)
-                {
-                    array[i] = reader.ReadInt16();
-                }
-
-                int k = array[300];
-            }
+          array[i] = reader.ReadInt16();
         }
-        //public static int X, Y, Z;
-        //public static short[]? array;
-        //
-        //public static void readBin(string path)
-        //{
-        //    if (File.Exists(path))
-        //    {
-        //        BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
-        //        X = reader.ReadInt32();
-        //        Y = reader.ReadInt32();
-        //        Z = reader.ReadInt32();
-        //        int arraySize = X * Y * Z;
-        //        array = new short[arraySize];
-        //        for (int i = 0; i < arraySize; i++)
-        //        {
-        //            array[i] = reader.ReadInt16();
-        //        }
-        //    }
-        //}
+
+        int k = array[300];
+      }
     }
-    public static class View
+    //public static int X, Y, Z;
+    //public static short[]? array;
+    //
+    //public static void readBin(string path)
+    //{
+    //    if (File.Exists(path))
+    //    {
+    //        BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+    //        X = reader.ReadInt32();
+    //        Y = reader.ReadInt32();
+    //        Z = reader.ReadInt32();
+    //        int arraySize = X * Y * Z;
+    //        array = new short[arraySize];
+    //        for (int i = 0; i < arraySize; i++)
+    //        {
+    //            array[i] = reader.ReadInt16();
+    //        }
+    //    }
+    //}
+  }
+  public static class View
+  {
+    static int VBOtexture; //хранит номер текстуры в памяти видеокарты
+    static Bitmap textureImage;
+
+    public static void SetupView(int width, int height)
     {
-        public static void SetupView(int width, int height)
-        {
-            GL.ShadeModel(ShadingModel.Smooth);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadIdentity();
-            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
-            GL.Viewport(0, 0, width, height);
-        }
-        public static Color TransferFunction(short value)
-        {
-            int min = 0;
-            int max = 255;
-            int newVal = Math.Clamp((value - min) * 255 / (max - min), 0, 255);
-            return Color.FromArgb(newVal, newVal, newVal);
-        }
-        public static void DrawQuads(int layerNumber)
-        {
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            GL.Begin(BeginMode.Quads);
-            for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
-                for (int y_coord = 0; y_coord < Bin.Y - 1; y_coord++)
-                {
-                    short value;
-                    value = Bin.array[x_coord + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord, y_coord);
-                    value = Bin.array[x_coord + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord, y_coord + 1);
-                    value = Bin.array[(x_coord + 1) + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord + 1, y_coord + 1);
-                    value = Bin.array[(x_coord + 1) + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord + 1, y_coord);
-                }
-            GL.End();
-        }
+      GL.ShadeModel(ShadingModel.Smooth);
+      GL.MatrixMode(MatrixMode.Projection);
+      GL.LoadIdentity();
+      GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+      GL.Viewport(0, 0, width, height);
     }
+    public static Color TransferFunction(short value)
+    {
+      int min = 0;
+      int max = 255;
+      int newVal = Math.Clamp((value - min) * 255 / (max - min), 0, 255);
+      return Color.FromArgb(newVal, newVal, newVal);
+    }
+    public static void DrawQuads(int layerNumber)
+    {
+      GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+      GL.Begin(BeginMode.Quads);
+      for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
+        for (int y_coord = 0; y_coord < Bin.Y - 1; y_coord++)
+        {
+          short value;
+          value = Bin.array[x_coord + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
+          GL.Color3(TransferFunction(value));
+          GL.Vertex2(x_coord, y_coord);
+          value = Bin.array[x_coord + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+          GL.Color3(TransferFunction(value));
+          GL.Vertex2(x_coord, y_coord + 1);
+          value = Bin.array[(x_coord + 1) + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+          GL.Color3(TransferFunction(value));
+          GL.Vertex2(x_coord + 1, y_coord + 1);
+          value = Bin.array[(x_coord + 1) + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
+          GL.Color3(TransferFunction(value));
+          GL.Vertex2(x_coord + 1, y_coord);
+        }
+      GL.End();
+    }
+
+    public static void Load2DTexture()
+    {
+      GL.BindTexture(TextureTarget.Texture2D, VBOtexture);
+      BitmapData data = textureImage.LockBits(new System.Drawing.Rectangle(0, 0, textureImage.Width, textureImage.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+      GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+      textureImage.UnlockBits(data);
+      GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+      GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+      ErrorCode Er = GL.GetError();
+      string str = Er.ToString();
+    }
+
+    public static void generateTextureImage(int layerNumber)
+    {
+      textureImage = new Bitmap(Bin.X, Bin.Y);
+      for(int i = 0; i < Bin.X; ++i)
+      {
+        for(int j = 0; j < Bin.Y; ++j)
+        {
+          int pixelNumber = i + j * Bin.X + layerNumber * Bin.X * Bin.Y;
+          textureImage.SetPixel(i, j, TransferFunction(Bin.array[pixelNumber]));
+        }
+      }
+    }
+
+    public static void DrawTexture()
+    {
+      GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+      GL.Enable(EnableCap.Texture2D);
+      GL.BindTexture(TextureTarget.Texture2D, VBOtexture);
+
+      GL.Begin(BeginMode.Quads);
+      GL.Color3(Color.White);
+      GL.TexCoord2(0f,0f);
+      GL.Vertex2(0,0);
+      GL.TexCoord2(0f, 1f);
+      GL.Vertex2(0, Bin.Y);
+      GL.TexCoord2(1f, 1f);
+      GL.Vertex2(Bin.X,Bin.Y);
+      GL.TexCoord2(1f, 0f);
+      GL.Vertex2(Bin.X, 0);
+      GL.End();
+
+      GL.Disable(EnableCap.Texture2D);
+    }
+    
+    
+  }
 }
